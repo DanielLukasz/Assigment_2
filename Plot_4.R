78edf0e7b1d44d91c052247c4d05cc4c9815e4d2
DanielLukasz <0130@interia.pl>
2015-11-22 15:20:40 +0100

    Update Plot_4.R

--- Plot_4.R
+++ Plot_4.R
@@ -1,29 +1,24 @@
-# Load ggplot2 library
-require(ggplot2)
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
 
-# Loading provided datasets - loading from local machine
-NEI <- readRDS("D:/Dane/10231133/Documents/COURSERA/assigment2/summarySCC_PM25.rds")
-SCC <- readRDS("D:/Dane/10231133/Documents/COURSERA/assigment2/Source_Classification_Code.rds")
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
 
-# Coal combustion related sources
-SCC.coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),]
+png("plot4.png",width=480,height=480,units="px",bg="transparent")
 
-# Merge two data sets
-merge <- merge(x=NEI, y=SCC.coal, by='SCC')
-merge.sum <- aggregate(merge[, 'Emissions'], by=list(merge$year), sum)
-colnames(merge.sum) <- c('Year', 'Emissions')
+library(ggplot2)
 
-# Across the United States, how have emissions from coal combustion-related sources 
-# changed from 1999-2008?
+ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
+  geom_bar(stat="identity",fill="grey",width=0.75) +
+  theme_bw() +  guides(fill=FALSE) +
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
 
-# Generate the graph in the same directory as the source code
-png(filename='D:/Dane/10231133/Documents/COURSERA/assigment2/plot4.png')
-
-ggplot(data=merge.sum, aes(x=Year, y=Emissions/1000)) + 
-    geom_line(aes(group=1, col=Emissions)) + geom_point(aes(size=2, col=Emissions)) + 
-    ggtitle(expression('Total Emissions of PM'[2.5])) + 
-    ylab(expression(paste('PM', ''[2.5], ' in kilotons'))) + 
-    geom_text(aes(label=round(Emissions/1000,digits=2), size=2, hjust=1.5, vjust=1.5)) + 
-    theme(legend.position='none') + scale_colour_gradient(low='black', high='red')
+print(ggp)
 
 dev.off()
