0a973b1e597cca95117fab1dd28125dbbf4baf60
DanielLukasz <0130@interia.pl>
2015-11-22 15:19:49 +0100

    Update Plot_3.R

--- Plot_3.R
+++ Plot_3.R
@@ -1,28 +1,24 @@
-# Load ggplot2 library
-> require(ggplot2)
- 
-> # Loading provided datasets - loading from local machine
-> NEI <- readRDS("D:/Dane/10231133/Documents/COURSERA/assigment2/summarySCC_PM25.rds")
-> SCC <- readRDS("D:/Dane/10231133/Documents/COURSERA/assigment2/Source_Classification_Code.rds")
-> 
-> # Sampling
-> NEI_sampling <- NEI[sample(nrow(NEI), size=5000, replace=F), ]
-> 
-> # Baltimore City, Maryland == fips
-> MD <- subset(NEI, fips == 24510)
-> MD$year <- factor(MD$year, levels=c('1999', '2002', '2005', '2008'))
-> 
-> # Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, 
-> # which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? 
-> # Which have seen increases in emissions from 1999–2008? 
-> # Use the ggplot2 plotting system to make a plot answer this question.
-> 
-> # Generate the graph in the same directory as the source code
-> png(filename='D:/Dane/10231133/Documents/COURSERA/assigment2/plot3.png', width=800, height=500, units='px')
-> 
-> ggplot(data=MD, aes(x=year, y=log(Emissions))) + facet_grid(. ~ type) + guides(fill=F) +
-+     geom_boxplot(aes(fill=type)) + stat_boxplot(geom ='errorbar') +
-+     ylab(expression(paste('Log', ' of PM'[2.5], ' Emissions'))) + xlab('Year') + 
-+     ggtitle('Emissions per Type in Baltimore City, Maryland') +
-+     geom_jitter(alpha=0.10)
-> dev.off()
+# Load the NEI & SCC data frames.
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Subset NEI data by Baltimore's fip.
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+# Aggregate using sum the Baltimore emissions data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot3.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
